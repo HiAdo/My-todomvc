5a4abcea444b7ce5f0c1a19b751db7ac16a43cf3
HiAdo <jeffduuu@gmail.com>
2020-03-19 23:06:08 +0700

    切换所有任务状态

--- js/app.js
+++ js/app.js
@@ -22,17 +22,29 @@
 			items,
 		},
 		computed: {
-			remaining(){
+			toggleAll: {
+				get() {
+					console.log("当get方法里面的属性性发生变化时，更新toggleAll值");
+					return this.remaining === 0;  // 当剩余任务为0时，返回true，即更新toggleAll复选框value为true
+				},
+				set(newStatus) {  // 当toggleAll属性发生变化时，调用setter，将每个任务的completed的值都设置和toggleAll一样
+					console.log("当toggleAll属性变化时，调用set方法");
+					this.items.forEach(item => {
+						item.completed = newStatus
+					});
+				}
+			},
+			remaining() {
 				// 过滤未完成的任务(未完成的为fase, !为true，true的才被过滤出来)
 				const unFinishItems = this.items.filter(item => !item.completed)
 				return unFinishItems.length
 			}
 		},
 		methods: {
-			addItem(event){
+			addItem(event) {
 				// console.log(event.target.value);  // 获取文本输出框的值
 				const content = event.target.value.trim()
-				if (!content.length){  // 值为空，则0，那么返回false，!则为true，直接不做处理
+				if (!content.length) {  // 值为空，则0，那么返回false，!则为true，直接不做处理
 					return
 				}
 				const id = items.length + 1
